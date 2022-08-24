fd69d7d98fdf58d0335200ff87b821c18ce359b8
NandaReisVerissimo <nandareis83@gmail.com>
2022-08-24 16:27:30 -0300

    dados preenchidos pratica 1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,15 @@ let email = "nandareis83@gmail.com"
 let profissao = "Estudante de Programação"
 let idade = 38
 let cidadeDosSonhos = "Jerusalém"
+
+console.log ({nome,email,profissao,idade,cidadeDosSonhos})
+console.log ("Nome:", nome, 
+"E-mail:", email, 
+"Profissão:", profissao, 
+"Idade:", idade, 
+"Cidade:", cidadeDosSonhos)
+console.log (nome)
+console.log (email)
+console.log (profissao)
+console.log (idade)
+console.log (cidadeDosSonhos)
