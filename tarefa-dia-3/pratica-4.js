06ba51a856ff318afa39125ed774a07a1aebd05b
NandaReisVerissimo <nandareis83@gmail.com>
2022-08-25 22:37:24 -0300

    número primo

---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,15 @@
+// Agora é com você! Crie um código no arquivo pratica-4.js que começando com a entrada de um número inteiro maior que zero, exiba Sim, o número ${numero} é primo! se este número for primo e Não, o número ${numero} não é primo! se ele não for um número primo. Como sugestão, pode colocar o nome da variável que vai armazenar esse valor de entrada de numero. Veja abaixo alguns exemplos.
+
+let i = 2
+const n = 1500450271
+let ehPrimo = true
+
+if (n % i != 0) {
+    console.log (`Sim, o número ${n} é primo!`)
+} else {
+    console.log (`Não, o número ${n} não é primo!`)
+}
+
+
+//Sim, o número ${numero} é primo!
+//Não, o número ${numero} não é primo!
